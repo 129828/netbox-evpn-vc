c24b65b8f521d2edd16dca139084e297de0b1a47
Joris Janssen <joris.janssen@vancis.nl>
2023-09-21 10:20:48 +0200

    better menu

--- netbox_evpn_vc/navigation.py
+++ netbox_evpn_vc/navigation.py
@@ -1,4 +1,4 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem
+from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 from utilities.choices import ButtonColorChoices
 
 evpnvc_type_buttons = [
@@ -27,20 +27,46 @@ evpnvcvlan_buttons = [
     )
 ]
 
-menu_items = (
-    PluginMenuItem(
-        link='plugins:netbox_evpn_vc:evpnvc_list',
-        link_text='VCs',
-        buttons=evpnvc_buttons
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_evpn_vc:evpnvcvlan_list',
-        link_text='VC-VLANs',
-        buttons=evpnvcvlan_buttons
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_evpn_vc:evpnvctype_list',
-        link_text='VC Types',
-        buttons=evpnvc_type_buttons
-    ),
+# menu_items = (
+#     PluginMenuItem(
+#         link='plugins:netbox_evpn_vc:evpnvc_list',
+#         link_text='VCs',
+#         buttons=evpnvc_buttons
+#     ),
+#     PluginMenuItem(
+#         link='plugins:netbox_evpn_vc:evpnvcvlan_list',
+#         link_text='VC-VLANs',
+#         buttons=evpnvcvlan_buttons
+#     ),
+#     PluginMenuItem(
+#         link='plugins:netbox_evpn_vc:evpnvctype_list',
+#         link_text='VC Types',
+#         buttons=evpnvc_type_buttons
+#     ),
+# )
+
+menu = PluginMenu(
+    label='EVPN',
+    icon_class='mdi mdi-vpn',
+    groups=(
+        (
+            'EVPN',
+            PluginMenuItem(
+                link='plugins:netbox_evpn_vc:evpnvc_list',
+                link_text='VCs',
+                buttons=evpnvc_buttons
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_evpn_vc:evpnvcvlan_list',
+                link_text='VC-VLANs',
+                buttons=evpnvcvlan_buttons
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_evpn_vc:evpnvctype_list',
+                link_text='VC Types',
+                buttons=evpnvc_type_buttons
+            ),
+        ),
+
+    )
 )
