799483062d1783b6a635f822096dcb5563154c9d
Joris Janssen <jorisj@vancis.nl>
2025-04-17 15:49:26 +0200

    migrate to v4

--- netbox_evpn_vc/graphql.py
+++ netbox_evpn_vc/graphql.py
@@ -1,5 +1,7 @@
 import strawberry
 import strawberry_django
+
+from netbox.graphql.filter_mixins import autotype_decorator, BaseFilterMixin
 from typing import Optional, Annotated, List
 from . import models
 
@@ -13,7 +15,13 @@ class EvpnVCType:
     pass
 
 
-@strawberry_django.type(models.EvpnVCVlan, fields="__all__", filters=EvpnVCVlanFilterSet)
+@strawberry_django.filter(models.EvpnVCVlan, lookups=True)
+@autotype_decorator(EvpnVCVlanFilterSet)
+class EvpnVCVlanFilter(BaseFilterMixin):
+    pass
+
+
+@strawberry_django.type(models.EvpnVCVlan, fields="__all__", filters=EvpnVCVlanFilter)
 class EvpnVCVlanType:
     evpn_vc: Optional[EvpnVCType]
     vlan: Annotated["VLANType", strawberry.lazy("ipam.graphql.types")]
@@ -24,25 +32,19 @@ class EvpnVCTypeType:
     pass
 
 
-@strawberry.type
-class Query:
-    @strawberry.field
-    def evpn_vcs(self, id: int) -> EvpnVCType:
-        return None
+@strawberry.type(name="Query")
+class EVPNQuery:
+
+    evpn_vcs: EvpnVCType = strawberry_django.field()
     evpn_vcs_list: list[EvpnVCType] = strawberry_django.field()
 
-    @strawberry.field
-    def evpn_vc_vlans(self, id: int) -> EvpnVCVlanType:
-        return models.EvpnVCVlan.objects.filter(id=id).first()
-
+    evpn_vc_vlans: EvpnVCVlanType = strawberry_django.field()
     evpn_vc_vlans_list: list[EvpnVCVlanType] = strawberry_django.field()
 
-    @strawberry.field
-    def evpn_vc_types(self, id: int) -> EvpnVCTypeType:
-        return None
+    evpn_vc_types: EvpnVCTypeType = strawberry_django.field()
     evpn_vc_types_list: list[EvpnVCTypeType] = strawberry_django.field()
 
 
 schema = [
-    Query,
+    EVPNQuery,
 ]
