5e2798992662ab2a90c4ea22ab2efaf9a9b0df97
Joris Janssen <jorisj@vancis.nl>
2025-04-17 11:18:00 +0200

    migrate to v4

--- netbox_evpn_vc/__init__.py
+++ netbox_evpn_vc/__init__.py
@@ -1,4 +1,5 @@
 from netbox.plugins import PluginConfig
+from .graphql import Query as EvpnQuery
 
 class NetBoxEvpnVCConfig(PluginConfig):
     name = 'netbox_evpn_vc'
@@ -6,6 +7,7 @@ class NetBoxEvpnVCConfig(PluginConfig):
     description = 'Manage EVPN Virtual Circuit'
     version = '0.2'
     base_url = 'evpn-vc'
-    min_version = '3.2.0'
+    min_version = '3.2.0',
+    graphql_types = [EvpnQuery]
 
 config = NetBoxEvpnVCConfig
