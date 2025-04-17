dc2d78e6dd860caab1ad89c9bbb686d47ade8543
Joris Janssen <jorisj@vancis.nl>
2025-04-17 11:20:51 +0200

    migrate to v4

--- netbox_evpn_vc/graphql.py
+++ netbox_evpn_vc/graphql.py
@@ -1,22 +1,33 @@
 import strawberry
-from netbox.graphql.query import ObjectQuery
 from strawberry_django.type import type as django_type
 from . import models, filtersets
+
 
 @django_type(models.EvpnVC)
 class EvpnVCType:
     pass
 
+
 @django_type(models.EvpnVCVlan, filters=filtersets.EvpnVCVlanFilterSet)
 class EvpnVCVlanType:
     pass
+
 
 @django_type(models.EvpnVCType)
 class EvpnVCTypeType:
     pass
 
+
 @strawberry.type
 class Query:
-    evpn_vc: ObjectQuery[EvpnVCType] = ObjectQuery(model=models.EvpnVC)
-    evpn_vc_vlan: ObjectQuery[EvpnVCVlanType] = ObjectQuery(model=models.EvpnVCVlan, filterset_class=filtersets.EvpnVCVlanFilterSet)
-    evpn_vc_type: ObjectQuery[EvpnVCTypeType] = ObjectQuery(model=models.EvpnVCType)
+    @strawberry.field
+    def evpn_vcs(self) -> list[EvpnVCType]:
+        return models.EvpnVC.objects.all()
+
+    @strawberry.field
+    def evpn_vc_vlans(self) -> list[EvpnVCVlanType]:
+        return models.EvpnVCVlan.objects.all()
+
+    @strawberry.field
+    def evpn_vc_types(self) -> list[EvpnVCTypeType]:
+        return models.EvpnVCType.objects.all()
