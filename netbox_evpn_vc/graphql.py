8c4047693b0c235639b8c0d852e4ca59884e765f
Joris Janssen <jorisj@vancis.nl>
2025-04-17 15:24:14 +0200

    migrate to v4

--- netbox_evpn_vc/graphql.py
+++ netbox_evpn_vc/graphql.py
@@ -12,7 +12,7 @@ class EvpnVCType:
 @strawberry_django.type(models.EvpnVCVlan, fields="__all__")
 class EvpnVCVlanType:
     evpn_vc: Optional[EvpnVCType]
-    evpnvcvlan: Annotated["VlanType", strawberry.lazy("ipam.graphql.types")]
+    vlan: Annotated["VLANType", strawberry.lazy("ipam.graphql.types")]
 
 
 @strawberry_django.type(models.EvpnVCType, fields="__all__")
