0449ac2659081fe65749ffb798e92e3d07cb167b
Joris <jorisshukojanssen@gmail.com>
2026-01-07 14:14:08 +0100

    v4.5 fix?

--- netbox_evpn_vc/graphql.py
+++ netbox_evpn_vc/graphql.py
@@ -2,7 +2,7 @@ import strawberry
 import strawberry_django
 
 # from netbox.graphql.filter_mixins import autotype_decorator, BaseFilterMixin
-from netbox.graphql.filter_mixins import BaseFilterMixin
+from netbox.graphql.filter_mixins import BaseFilter
 from typing import Optional, Annotated, List
 from netbox.graphql.types import NetBoxObjectType
 from . import models
@@ -19,7 +19,7 @@ class EvpnVCType(NetBoxObjectType):
 
 @strawberry_django.filter(models.EvpnVCVlan, lookups=True)
 # @autotype_decorator(EvpnVCVlanFilterSet)
-class EvpnVCVlanFilter(BaseFilterMixin):
+class EvpnVCVlanFilter(BaseFilter):
     vlan_id: Optional[List[strawberry.ID]]
     evpn_vc_id: Optional[List[strawberry.ID]]
 
